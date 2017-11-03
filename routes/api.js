a4d92b7318d21b5afb156a39f91d1751148a75af
panforrest <guoqianp@gmail.com>
2017-11-02 21:53:55 -0400

    1:1:34 integrate twilio number call :3000/api/sms get data:SMeda780f8d7bf465b8ec0869a7f51f033

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,38 @@ const twilio = require('twilio')
 
 /*  This is a sample API route. */
 router.get('/:sms', function(req, res){
-	res.json({
-		confirmation: 'success',
-		message: 'this is the sms route'
+	// res.json({
+	// 	confirmation: 'success',
+	// 	message: 'this is the sms route'
+	// })
+	var accountSid = 'AC4b99f76eaaec3adff9b44c733bdc00b6' // Your Account SID from www.twilio.com/console
+	var authToken = 'abdedad29ab5a58bc45cb068386abaab' // Your Auth Token from www.twilio.com/console
+
+	var twilio = require('twilio')
+	var client = new twilio(accountSid, authToken)
+
+	let payload = null
+	client.messages.create({
+	    body: 'This is a TEST message!',
+	    to: '+19089061042',  // Text this number
+	    from: '+19085164650' // From a valid Twilio number
 	})
+	.then((message) => {
+		payload = {
+			confirmation: 'success',
+			data: message.sid
+		}
+
+		res.json(payload)
+		return
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+
 })
 
 router.get('/:resource', function(req, res){
